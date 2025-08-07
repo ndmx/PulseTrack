1fbee8540e44411434967a3bd95efe7de097ab85
ndmx <alexanderukaga@gmail.com>
2025-08-07 19:58:05 -0400

    fix: correct import to db.connect in db/load.py

--- db/load.py
+++ db/load.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from db_connect import engine
+from db.connect import engine
 def load_to_db(df, table_name):
     df.to_sql(table_name, engine, if_exists='append', index=False)
 def load_approvals(df_approvals):
