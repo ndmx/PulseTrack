62572521f370bbee183161343f246cf0f0bd4d65
ndmx <alexanderukaga@gmail.com>
2025-08-11 19:22:48 -0400

    web: add responsive container padding (30px on wide screens)

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import App from "./app"
+import "./styles.css"
 
 const qc = new QueryClient()
 ReactDOM.createRoot(document.getElementById("root")!).render(
